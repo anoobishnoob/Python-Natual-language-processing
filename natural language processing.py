3b92df382b20eca2e0ddff7cce42b4d494120143
Samme Qandil <Saqandil@gmail.com>
2018-11-17 00:26:05 -0800

    first example with tokenizing

--- natural language processing.py
+++ natural language processing.py
@@ -1,3 +1,16 @@
 import nltk
-nltk.download("all")
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+
+example_text = "Hello there, how are you doing today? The weather is great and Python is awesome."
+
+print(sent_tokenize(example_text))
+print(word_tokenize(example_text))
+
+
+for i in word_tokenize(example_text):
+    print(i)
+
+
+
 
