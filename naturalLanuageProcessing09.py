7181dbdd5ed771d89b8d6b00732e06580820657f
Samme Qandil <Saqandil@gmail.com>
2018-11-20 00:15:55 -0800

    Update naturalLanuageProcessing09.py

--- naturalLanuageProcessing09.py
+++ naturalLanuageProcessing09.py
@@ -1,3 +1,8 @@
 import nltk
+from nltk.corpus import gutenberg
 
-print(nltk.__file__)  # very useful for just finding file paths
+
+# print(nltk.__file__) very useful for just finding file paths
+
+
+
