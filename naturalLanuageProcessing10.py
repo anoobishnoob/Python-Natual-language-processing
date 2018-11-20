f7e85f4bed5cc22251a694611c36b5bdb3d81e5e
Samme Qandil <Saqandil@gmail.com>
2018-11-20 00:44:06 -0800

    Wordnet and word similarity

---
+++ naturalLanuageProcessing10.py
@@ -0,0 +1,50 @@
+from nltk.corpus import wordnet
+
+syns = wordnet.synsets("program")  #synonyms
+
+print(syns)
+print(syns[0])
+print(syns[0].lemmas())
+print(syns[0].lemmas()[0].name())
+
+
+print(syns[0].definition())
+print(syns[0].examples())
+
+
+
+
+antonyms = []
+synonyms = []
+
+for syn in wordnet.synsets("good"):
+    for l in syn.lemmas():
+        #print("l:", l) for debugging
+        synonyms.append(l.name())
+        if l.antonyms():
+            antonyms.append(l.antonyms()[0].name())
+
+
+
+# print(set(synonyms))
+# print(set(antonyms))
+
+
+
+w1 = wordnet.synset("ship.n.01")
+w2 = wordnet.synset("boat.n.01")
+
+print(w1.wup_similarity(w2))
+
+
+
+w1 = wordnet.synset("ship.n.01")
+w2 = wordnet.synset("car.n.01")
+
+print(w1.wup_similarity(w2))
+
+
+w1 = wordnet.synset("ship.n.01")
+w2 = wordnet.synset("cat.n.01")
+
+print(w1.wup_similarity(w2))
