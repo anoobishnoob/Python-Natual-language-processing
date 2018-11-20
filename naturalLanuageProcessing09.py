2e9d467069d09fc3226c8749705e6d84cd17e59a
Samme Qandil <Saqandil@gmail.com>
2018-11-20 00:20:23 -0800

    just messing with NLTK.corpora

--- naturalLanuageProcessing09.py
+++ naturalLanuageProcessing09.py
@@ -1,8 +1,13 @@
 import nltk
 from nltk.corpus import gutenberg
+from nltk.tokenize import sent_tokenize
 
 
 # print(nltk.__file__) very useful for just finding file paths
 
 
+sample = gutenberg.raw("bible-kjv.txt")
+
+tok = sent_tokenize(sample)
+print(tok[5:15])
 
