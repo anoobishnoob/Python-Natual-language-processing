d39d16e526773e77b82a245ea4110b4fecbe6af9
Samme Qandil <Saqandil@gmail.com>
2018-11-21 01:57:23 -0800

    Text Classification

--- naturalLanuageProcessing11.py
+++ naturalLanuageProcessing11.py
@@ -6,4 +6,13 @@ documents = [(list(movie_reviews.words(fileid)), category)
              for category in movie_reviews.categories()
              for fileid in movie_reviews.fileids(category)]
 random.shuffle(documents)
-print(documents[1])
+#print(documents[1])
+
+all_words = []
+for w in movie_reviews.words():
+    all_words.append(w.lower())
+
+all_words = nltk.FreqDist(all_words)
+print(all_words.most_common(15))
+
+print(all_words["stupid"])
