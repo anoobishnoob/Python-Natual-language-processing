d9a2a9e6fe81f940f25e85d3f2be2897ea2b37a1
Samme Qandil <Saqandil@gmail.com>
2018-11-21 01:48:37 -0800

    Update naturalLanuageProcessing11.py

--- naturalLanuageProcessing11.py
+++ naturalLanuageProcessing11.py
@@ -2,3 +2,16 @@ import nltk
 import random
 from nltk.corpus import movie_reviews
 
+documents = [(list(movie_reviews.words(fileid)), category)
+             for category in movie_reviews.categories()
+             for fileid in movie_reviews.fileids(category)]
+random.shuffle(documents)
+print(documents[1])
+
+'''
+same thing as above
+documents = []
+for category in movie_reviews.categories():
+    for fileid in movie_reviews.fileids(category):
+        documents.append(list(movie_reviews.words(fileid)), category)
+'''
