5a30b908c67c726536dd8f8db357f35480c026d2
Samme Qandil <Saqandil@gmail.com>
2018-11-21 01:48:56 -0800

    Update naturalLanuageProcessing11.py

--- naturalLanuageProcessing11.py
+++ naturalLanuageProcessing11.py
@@ -7,11 +7,3 @@ documents = [(list(movie_reviews.words(fileid)), category)
              for fileid in movie_reviews.fileids(category)]
 random.shuffle(documents)
 print(documents[1])
-
-'''
-same thing as above
-documents = []
-for category in movie_reviews.categories():
-    for fileid in movie_reviews.fileids(category):
-        documents.append(list(movie_reviews.words(fileid)), category)
-'''
